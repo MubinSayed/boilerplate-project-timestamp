c3dacae1b45945b7266d33819154999793411da8
MubinSayed <mbnsayed@gmail.com>
2023-10-08 10:37:23 +0000

    fixes

--- index.js
+++ index.js
@@ -20,27 +20,27 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
+
 app.get("/api/:date?", function (req, res) {
-  const dateParam = req.params.date;
-  let date;
+  let date = new Date(req.params.date);
 
-  if (!dateParam) {
-    // If the date parameter is empty, return the current time
-    date = new Date();
-  } else {
-    // Try to parse the provided date string (assuming it's in milliseconds)
-    date = new Date(parseInt(dateParam)); // Parse as an integer and multiply by 1000
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
   }
 
-  if (isNaN(date)) {
-    // If the date is invalid, return an error object
-    return res.json({ error: "Invalid Date" });
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"});
+    return;
   }
-
+  
   // Return a JSON object with Unix timestamp and UTC date string
   res.json({
     unix: date.getTime(),
